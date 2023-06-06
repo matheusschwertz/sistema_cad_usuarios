1546f6c12de9afe07495b19c23e95b115870887e
matheusschwertz <matheusschwertz@gmail.com>
2023-06-06 19:43:16 -0300

    first

---
+++ projeto_cad_usuarios/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    # rota, view resposável, nome referência
+    # usuarios.com
+    path('',views.home,name='home'),
+    
+]
